62ad7fa41b965f52188cc513643e6fa416130199
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-01-23 04:13:11 +0000

    added questions to question.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,64 @@
-var me = "Gene"
+var questions = [
+    {
+    question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    choices: ["Position", "Window", "Standard", "Location"],
+    answer: "Window"
+},
+    {
+    question: "Which of the following can be used to call a JavaScript Code Snippet?",
+    choices: ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
+    answer: "Function/Method"
+},
+
+    {
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    choices: ["It is an ordered list of values", "It is an ordered list of objects", "It is an ordered list of string", "It is an ordered list of functions"],
+    answer: "It is an ordered list of values"
+},
+
+    {
+    question: "The statement a===b refers to _________",
+    choices: [ "Both a and b are equal in value, type and reference address", "Both a and b are equal in value", "Both a and b are equal in value and type", "There is no such statement"],
+    answer: "Both a and b are equal in value and type"
+},
+
+    {
+    question: "Among the keywords below, which one is not a statement?",
+    choices: ["debugger", "with", "if", "use strict"],
+    answer: "use strict"
+},
+{
+    question: "Why is event handlers needed in JS?",
+    choices: ["Allows JavaScript code to alter the behaviour of windows", "Adds innerHTML page to the code", "Change the server location", "Performs handling of exceptions and occurrences"],
+    answer: "Allows JavaScript code to alter the behaviour of windows"
+},
+    {
+    question:"A function definition expression can be called as __________",
+    choices: ["Function prototype", "Function literal", "Function calling", "Function declaration"],
+    answer: "Function literal"
+},
+
+    {
+    question: "Among the following, which one is a ternary operator?", 
+    choices: ["+", " :", "–", "?:"],
+    answer: "?:"
+},
+
+    {
+    question: "A conditional expression is also called a _______________",
+    choices: ["Alternative to if-else", "Immediate if", "If-then-else statement", "Switch statement"],
+    answer: "Immediate if"
+},
+
+    {
+    question: "Which of the operator is used to test if a particular property exists or not?",
+    choices: ["in", "exist", "within", "exists"],
+    answer: "in"
+}
+]
+
+for (i of questions) {
+    console.log(i.question)
+}
+
+
