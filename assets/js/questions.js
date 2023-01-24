b9dae25d589755fca456501b57d0336ac69abb8e
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-01-24 16:49:39 +0000

    edited questions in question bank array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,59 +1,59 @@
 var questionBank = [
     {
     question: "1. Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
-    choices: ["Position", "Window", "Standard", "Location"],
-    answer: "Window"
+    choices: ["a) Position", "b) Window", "c) Standard", "d) Location"],
+    answer: "b) Window"
 },
     {
     question: "2. Which of the following can be used to call a JavaScript Code Snippet?",
-    choices: ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
-    answer: "Function/Method"
+    choices: ["a) Function/Method", "b) Preprocessor", "c) Triggering Event", "d) RMI"],
+    answer: "a) Function/Method"
 },
 
     {
     question: "3. Arrays in JavaScript are defined by which of the following statements?",
-    choices: ["It is an ordered list of values", "It is an ordered list of objects", "It is an ordered list of string", "It is an ordered list of functions"],
-    answer: "It is an ordered list of values"
+    choices: ["a) It is an ordered list of values", "b) It is an ordered list of objects", "c) It is an ordered list of string", "d) It is an ordered list of functions"],
+    answer: "a) It is an ordered list of values"
 },
 
     {
     question: "4. The statement a===b refers to _________",
-    choices: [ "Both a and b are equal in value, type and reference address", "Both a and b are equal in value", "Both a and b are equal in value and type", "There is no such statement"],
-    answer: "Both a and b are equal in value and type"
+    choices: [ "a) Both a and b are equal in value, type and reference address", "b) Both a and b are equal in value", "c) Both a and b are equal in value and type", "d) There is no such statement"],
+    answer: "c) Both a and b are equal in value and type"
 },
 
     {
     question: "5. Among the keywords below, which one is not a statement?",
-    choices: ["debugger", "with", "if", "use strict"],
-    answer: "use strict"
+    choices: ["a) debugger", "b) with", "c) if", "d) use strict"],
+    answer: "d) use strict"
 },
 {
     question: "6. Why is event handlers needed in JS?",
-    choices: ["Allows JavaScript code to alter the behaviour of windows", "Adds innerHTML page to the code", "Change the server location", "Performs handling of exceptions and occurrences"],
-    answer: "Allows JavaScript code to alter the behaviour of windows"
+    choices: ["a) Allows JavaScript code to alter the behaviour of windows", "b) Adds innerHTML page to the code", "c) Change the server location", "d) Performs handling of exceptions and occurrences"],
+    answer: "a) Allows JavaScript code to alter the behaviour of windows"
 },
     {
     question:"7. A function definition expression can be called as __________",
-    choices: ["Function prototype", "Function literal", "Function calling", "Function declaration"],
-    answer: "Function literal"
+    choices: ["a) Function prototype", "b) Function literal", "c) Function calling", "d) Function declaration"],
+    answer: "b) Function literal"
 },
 
     {
     question: "8. Among the following, which one is a ternary operator?", 
-    choices: ["+", " :", "–", "?:"],
-    answer: "?:"
+    choices: ["a) +", "b) :", "c) –", "d) ?:"],
+    answer: "d) ?:"
 },
 
     {
     question: "9. A conditional expression is also called a _______________",
-    choices: ["Alternative to if-else", "Immediate if", "If-then-else statement", "Switch statement"],
-    answer: "Immediate if"
+    choices: ["a) Alternative to if-else", "b) Immediate if", "c) If-then-else statement", "d) Switch statement"],
+    answer: "b) Immediate if"
 },
 
     {
     question: "10. Which of the operator is used to test if a particular property exists or not?",
-    choices: ["in", "exist", "within", "exists"],
-    answer: "in"
+    choices: ["a) in", "b) exist", "c) within", "d) exists"],
+    answer: "a) in"
 }
 ]
 
